543c29ecc4728a017d6a4c9dd729199db386ba9a
akashVerma <akashv000@gmail.com>
2020-09-22 18:01:08 +0530

    version 1.0

--- src/User/Profile.js
+++ src/User/Profile.js
@@ -12,10 +12,13 @@ const Profile = (props) => {
         password: '',
         error: '',
         email: '',
+        street: '',
+        state: "",
+        city: '',
         success: false
     })
 
-    const { name, password, email, error, success } = values
+    const { name, password, email, city, street, state } = values
 
     const userId = isAuth()._id;
     const token = getCookie('token');
@@ -31,15 +34,20 @@ const Profile = (props) => {
             )
             .then(res => {
                 console.log(res.data)
-                setValues({ ...values, name: res.data.name, email: res.data.email })
+                setValues({
+                    ...values, name: res.data.name, email: res.data.email,
+                    city: res.data.Address.city, street: res.data.Address.street,
+                    state: res.data.Address.state
+                })
             })
             .catch(err => {
                 console.error(err);
                 setValues({ ...values, error: true })
             });
     }
-    
-    const updateProfile = (userId, token, user) => {
+
+    const updateProfile = (userId, token, email, password, name, street, city, state) => {
+        const user = { email, password, name, Address: { street, city, state } }
         axios
             .put(`${process.env.REACT_APP_API_URL}/user/${userId}`,
                 {
@@ -76,27 +84,39 @@ const Profile = (props) => {
 
     const clickSubmit = (e) => {
         e.preventDefault();
-        updateProfile(userId, token, { email, password, name });
+        updateProfile(userId, token, email, password, name, street, city, state);
     }
 
     return (
         <Fragment>
-        <Menu />
-        <Layout title='Update Profile' description={`Hey ${name} want to update your profile `}>
-            <Form onSubmit={clickSubmit}>
-                <Form.Group controlId="formBasicName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" onChange={handleChange('name')} value={name} placeholder="Enter email" />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" onChange={handleChange('email')} value={email} placeholder="Enter email" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                 </Button>
-            </Form>
-        </Layout>
+            <Menu />
+            <Layout title='Update Profile' description={`Hey ${name} want to update your profile `}>
+                <Form onSubmit={clickSubmit}>
+                    <Form.Group controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" onChange={handleChange('name')} value={name} placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" onChange={handleChange('email')} value={email} placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>Street</Form.Label>
+                        <Form.Control type="text" onChange={handleChange('street')} value={street} placeholder="Enter house number and street" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>City</Form.Label>
+                        <Form.Control type="text" onChange={handleChange('city')} value={city} placeholder="Enter city" />
+                    </Form.Group>
+                    <Form.Group >
+                        <Form.Label>State</Form.Label>
+                        <Form.Control type="text" onChange={handleChange('state')} value={state} placeholder="Enter State" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+         </Button>
+                </Form>
+            </Layout>
         </Fragment>
     );
 }
