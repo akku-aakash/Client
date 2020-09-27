de7845f09dcaea322b5bcff22b9ef96a6e952eb4
akashVerma <akashv000@gmail.com>
2020-09-27 19:14:42 +0530

    version 1.1

--- src/homeCore/Store.js
+++ src/homeCore/Store.js
@@ -1,10 +1,179 @@
 import React from 'react';
 import Menu from '../core/Menu'
+import { Helmet } from 'react-helmet';
+import { Container, Row, Col } from 'react-bootstrap';
+import Img1 from '../shimg/main.jpg'
+import Img4 from '../shimg/tit.jpg'
+import Img5 from '../shimg/tit1.jpg'
+import Img6 from '../shimg/tit2.jpg'
+import Img7 from '../shimg/am.jpg'
+import Img8 from '../shimg/am1.jpg'
+import Img9 from '../shimg/am2.png'
+import Img10 from '../shimg/pt.jpg'
+import Img11 from '../shimg/pt1.jpg'
+import Img12 from '../shimg/pt2.jpg'
+import Img13 from '../shimg/ya.jpg'
+import Img14 from '../shimg/ya1.jpg'
+import Img15 from '../shimg/ya2.jpg'
 
 const Store = () => {
     return (
         <div>
-            <Menu/>
+            <Helmet>
+                <title>Corporate Events</title>
+                <meta name="description" content="Buy online birthday decoration kit, diy decor box for an anniversary, baby shower and festivals. Explore unique gift ideas, birthday gifts, surprise gifts, virtual gifts. Book birthday and surprise planners, candlelight dinners, home surprises, and pan India experiences and activities for couples, families, and corporates." />
+                <meta name="keywords" content="diy decor box, birthday decoration kit, birthday decoration kit online, unique gifts, unique gift ideas, birthday gifts, anniversary gifts, surprise gifts, virtual gift ideas, virtual gifts online, surprise planners, surprise party planners, book surprises, book experiences " />
+                <meta name="author" content="Bunny Bash" />
+                <meta name="robots" content="index, follow"></meta>
+            </Helmet>
+            <Menu />
+            <div className="ssp">
+                <div className="home111">
+                    <div className="home114">
+                        <div className="home112">
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                        </div>
+                        <h1><i className="fa fa-star-half-o"></i></h1>
+                        <div className="home113">
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="ssp1">
+                    <Container fluid style={{ margin: "0px", padding: '0px', width: '100%' }}>
+                        <Row className="ssp111">
+                            <Col sm={12} lg={6}>
+                                <div className="sspppp2">
+                                    <img src={Img1} alt="fasfdhjk" />
+                                </div>
+                            </Col>
+                            <Col sm={12} lg={6}>
+                                <h3>Corporate Events</h3>
+                                <div className="ssp3">
+                                    <div className="ssp4">
+                                    <p>We at Bunny Bash, take store launch activity under corporate event management services. We are event planner for grand showroom launch, retail store grand opening, store relaunch activity across India with operational offices in Bangalore, Hyderabad and Mumbai.</p>
+                                    <p>A well-planned store grand opening shout out the presence of new partner in the business community. Main and must have attractions of the store launch features special guests, family members, festive decorations, store walkin incentives, purchase discounts and incentives, local marketing and family engagement to create sense of anticipation (#अपनी दुकान).</p>
+                                    <p>Many retail store, salons and restaurants also plan 'soft opening' to test the hiccups and streamline store level operations before the announcement of 'The Grand Opening'.</p>
+                                    <p>We at Bunny Bash has very experienced team of event and party planners working in this arena since 2010, and have launched 500+ small businesses, startup offices, co-working spaces, MNC offices, retail stores, retail showrooms, franchisee stores and shop-in-shop models across Indian states.</p>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="ssppp12">
+                    <h4>Titan Store/Showroom Launch Activity and Event Management:</h4>
+                    <Container fluid>
+                        <Row>
+                            <Col sm={12} md={6} lg={4} >
+                                <div className="ssppp14">
+                                    <img src={Img4} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Entrance Gate</h3>
+                                    <h6>Floral decoration at entrance gate</h6>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={6} lg={4} >
+                                <div className="ssppp14">
+                                    <img src={Img5} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Lighting Lamp</h3>
+                                    <h6>Lighting the lamp ceremony at Airport Entrance</h6>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={6} lg={4}>
+                                <div className="ssppp14">
+                                    <img src={Img6} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Inside Decoration</h3>
+                                    <h6>Ratan Tata sharing his thought and message inside the cabin</h6>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="ssppp12">
+                    <h4>Air Asia Route Launch Event Management</h4>
+                    <Container fluid>
+                        <Row>
+                            <Col sm={12} md={6} lg={4} >
+                                <div className="ssppp14">
+                                    <img src={Img9} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>BLR-Mumbai Route</h3>
+                                    <h6>THE PIONEER theme aircraft- dedicated to JRD TATA </h6>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={6} lg={4} >
+                                <div className="ssppp14">
+                                    <img src={Img7} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Lighting Lamp</h3>
+                                    <h6>Lighting the lamp ceremony at Airport Entrance</h6>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={6} lg={4}>
+                                <div className="ssppp14">
+                                    <img src={Img8} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Presence of Ratan TATA</h3>
+                                    <h6>Ratan Tata sharing his thought and message inside the cabin</h6>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="ssppp12">
+                    <h4>Pantaloons Mall Store Relaunch Activity And Decoration</h4>
+                    <Container fluid>
+                        <Row>
+                            <Col sm={6} md={5} lg={4} >
+                                <div className="ssppp13">
+                                    <div className="ssppp14">
+                                        <img src={Img10} alt="" />
+                                    </div>
+                                    <div className="ssppp14">
+                                        <img src={Img12} alt="" />
+                                    </div>
+                                </div>
+                            </Col>
+                            <Col sm={6} md={7} lg={8}>
+                                <div className="ssppp15">
+                                    <img src={Img11} alt="" />
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+                <div className="ssppp12">
+                    <h4>YLG Salon Launch Decoration Activity</h4>
+                    <Container fluid>
+                        <Row>
+                            <Col sm={12} md={6} lg={4} >
+                                <div className="ssppp14">
+                                    <img src={Img13} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Salon Mirror Decor</h3>
+                                    <h6>Mirror decor with balloon florals </h6>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={6} lg={4} >
+                                <div className="ssppp14">
+                                    <img src={Img14} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Salon Entrance Decor</h3>
+                                    <h6>Balloon pillars and ceiling frills</h6>
+                                </div>
+                            </Col>
+                            <Col sm={12} md={6} lg={4}>
+                                <div className="ssppp14">
+                                    <img src={Img15} alt="" />
+                                    <h3 style={{ textAlign: 'center', marginTop: '20px' }}>Spa Cabin Decor</h3>
+                                    <h6>Use of flower bouquets and balloons</h6>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            </div>
         </div>
     );
 };
